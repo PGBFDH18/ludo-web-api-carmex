e70cf13156e81906fb7e8d90b55906d135ea2c68
Phatchari <gataij@hotmail.com>
2019-01-24 21:07:10 +0100

    Lägg till en spelare

--- src/LudoWebAPI/Controllers/PlayersController.cs
+++ src/LudoWebAPI/Controllers/PlayersController.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LudoGameEngine;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LudoWebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/ludo")]
     [ApiController]
     public class PlayersController : ControllerBase
     {
@@ -17,17 +18,25 @@ namespace LudoWebAPI.Controllers
         {
             _games = games;
         }
-        // GET: api/Players
+        // GET: api/ludo/{gameid}/players
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
-        }      
+        }
 
-        // POST: api/Players
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // POST: api/ludo/{gameid}/players
+        [HttpPost("{gameid}/players")]
+        public JsonResult Post(int id, string name, int color)
         {
+            // hämtar spelet där spelaren skall skapas
+            LudoGame game = _games.GetOrCreateGame(id);
+
+            // lägg till en ny spelare till spelet
+            Player player = game.AddPlayer(name, (PlayerColor) color);
+
+            // retunera den nya spelaren
+            return new JsonResult(player);
         }       
     }
 }
